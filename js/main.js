e792443b91623dbecc3bb9eca0c281b8a6060809
ArleiRodrigues1 <arlei.freire011@gmail.com>
2023-03-13 19:14:38 -0300

    alterações finais 1

--- js/main.js
+++ js/main.js
@@ -12,25 +12,80 @@ function showPage() {
 
 
 // SWIPER
-var swiper = new Swiper('.about-content', {
-  slidesPerView: 1,
-  spaceBetween: 30,
+var swiper = new Swiper(".about-container", {
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   breakpoints: {
+    360: {
+      slidesPerView: 1.5,
+      spaceBetween: 50,
+    },
+    337: {
+      slidesPerView: 1.5,
+      spaceBetween: 100,
+    },
+    375: {
+      slidesPerView: 1.5,
+      spaceBetween: 50,
+    },
+    390: {
+      slidesPerView: 1.5,
+      spaceBetween: 50,
+    },
+    400: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
+    568: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    568: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    667: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
     640: {
       slidesPerView: 2,
-      spaceBetween: 15,
+      spaceBetween: 60,
     },
     768: {
       slidesPerView: 3,
-      spaceBetween: 15,
+      spaceBetween: 140,
     },
-    1000: {
+    996: {
       slidesPerView: 4,
-      spaceBetween: 20,
+      spaceBetween: 180,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 150,
+    },
+    1480: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+    1460: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+    1300: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 50,
     },
   },
 });
@@ -46,6 +101,7 @@ var swiper = new Swiper(".reviews-content", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  
 });
 
 
@@ -58,3 +114,21 @@ var swiper = new Swiper(".trataments-content", {
   },
 });
 
+
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
